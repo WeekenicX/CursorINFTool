ad8e5636e9828d56ad4899b30976e1d1c5c1d826
杰洛特 <sssvip666@qq.com>
2022-06-27 12:03:07 +0800

    添加刷新查找功能

--- CursorTool/Form1.Designer.cs
+++ CursorTool/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.languageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.中文ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,9 +56,8 @@
             this.lackLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lackLabel.Location = new System.Drawing.Point(37, 58);
             this.lackLabel.Name = "lackLabel";
-            this.lackLabel.Size = new System.Drawing.Size(103, 27);
+            this.lackLabel.Size = new System.Drawing.Size(0, 27);
             this.lackLabel.TabIndex = 1;
-            this.lackLabel.Text = "No Select";
             // 
             // menuStrip1
             // 
@@ -74,9 +74,12 @@
             // startToolStripMenuItem
             // 
             this.startToolStripMenuItem.AutoSize = false;
+            this.startToolStripMenuItem.BackColor = System.Drawing.Color.LightGray;
+            this.startToolStripMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.startToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.selectFolderToolStripMenuItem});
             this.startToolStripMenuItem.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.startToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Firebrick;
             this.startToolStripMenuItem.Name = "startToolStripMenuItem";
             this.startToolStripMenuItem.Size = new System.Drawing.Size(92, 40);
             this.startToolStripMenuItem.Text = "Start";
@@ -84,7 +87,7 @@
             // selectFolderToolStripMenuItem
             // 
             this.selectFolderToolStripMenuItem.Name = "selectFolderToolStripMenuItem";
-            this.selectFolderToolStripMenuItem.Size = new System.Drawing.Size(221, 32);
+            this.selectFolderToolStripMenuItem.Size = new System.Drawing.Size(224, 32);
             this.selectFolderToolStripMenuItem.Text = "Select Folder";
             this.selectFolderToolStripMenuItem.Click += new System.EventHandler(this.selectFolderToolStripMenuItem_Click);
             // 
@@ -102,25 +105,39 @@
             // englishToolStripMenuItem
             // 
             this.englishToolStripMenuItem.Name = "englishToolStripMenuItem";
-            this.englishToolStripMenuItem.Size = new System.Drawing.Size(197, 32);
+            this.englishToolStripMenuItem.Size = new System.Drawing.Size(224, 32);
             this.englishToolStripMenuItem.Text = "English   √";
             this.englishToolStripMenuItem.Click += new System.EventHandler(this.englishToolStripMenuItem_Click);
             // 
             // 中文ToolStripMenuItem
             // 
             this.中文ToolStripMenuItem.Name = "中文ToolStripMenuItem";
-            this.中文ToolStripMenuItem.Size = new System.Drawing.Size(197, 32);
+            this.中文ToolStripMenuItem.Size = new System.Drawing.Size(224, 32);
             this.中文ToolStripMenuItem.Text = "中文";
             this.中文ToolStripMenuItem.Click += new System.EventHandler(this.中文ToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::CursorTool.Properties.Resources.re;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(168, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1029, 585);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1029, 596);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lackLabel);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
@@ -142,5 +159,6 @@
         private ToolStripMenuItem languageToolStripMenuItem;
         private ToolStripMenuItem englishToolStripMenuItem;
         private ToolStripMenuItem 中文ToolStripMenuItem;
+        private Button button1;
     }
 }
